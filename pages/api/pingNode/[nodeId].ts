f4815736e9675d3bed96ed207d481213321e6640
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-15 20:59:58 +0100

    repo: formatting file
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- pages/api/pingNode/[nodeId].ts
+++ pages/api/pingNode/[nodeId].ts
@@ -1,13 +1,20 @@
 import axios from "axios";
-import {NextApiRequest, NextApiResponse} from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const {nodeId} = req.query
-        let ping = await (await axios.get(`${process.env.NEXT_PUBLIC_REST_URL}/network/ping/${nodeId}`)).data;
-        res.status(200).json({result: ping})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({error: error});
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { nodeId } = req.query;
+    let ping = await (
+      await axios.get(
+        `${process.env.NEXT_PUBLIC_REST_URL}/network/ping/${nodeId}`
+      )
+    ).data;
+    res.status(200).json({ result: ping });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
 }
--- pages/api/prices/[ticker].ts
+++ pages/api/prices/[ticker].ts
@@ -1,13 +1,20 @@
 import axios from "axios";
-import {NextApiRequest, NextApiResponse} from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const {ticker} = req.query
-        let price = await (await axios.get(`https://api.blockchain.com/v3/exchange/tickers/${ticker}`)).data;
-        res.status(200).json({price: price["last_trade_price"]})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({error: error});
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { ticker } = req.query;
+    let price = await (
+      await axios.get(
+        `https://api.blockchain.com/v3/exchange/tickers/${ticker}`
+      )
+    ).data;
+    res.status(200).json({ price: price["last_trade_price"] });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
 }
