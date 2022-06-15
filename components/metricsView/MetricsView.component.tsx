04bf78e64a900d529a7066c4644672dde742c764
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-15 20:59:58 +0100

    repo: formatting file
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- components/metricsView/MetricsView.component.tsx
+++ components/metricsView/MetricsView.component.tsx
@@ -1,7 +1,7 @@
-import {GetInfoNode} from "../../model/GetInfoNode";
+import { GetInfoNode } from "../../model/GetInfoNode";
 import Offline from "../genericView/Offline.component";
 import useSWR from "swr";
-import {fetcher} from "../../utils/AppUtils";
+import { fetcher } from "../../utils/AppUtils";
 import Loading from "../genericView/Loading.component";
 import React from "react";
 import Grid from "@mui/material/Grid";
@@ -9,45 +9,57 @@ import SummaryChannels from "./cards/SummaryChannels.components";
 import UpTimeNode from "./cards/UpTimeNode.component";
 import ChannelsPayments from "./cards/ChannelsPayments.component";
 import theme from "../../theme/DarkTheme";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 
 type MetricsViewProps = {
-    nodeInfo: GetInfoNode | null
-    show: (visible: boolean, message: string) => void
-}
+  nodeInfo: GetInfoNode | null;
+  show: (visible: boolean, message: string) => void;
+};
 
-export default function MetricsView({nodeInfo, show}: MetricsViewProps): JSX.Element {
-    const {data, error} = useSWR('/api/metrics', fetcher)
-    if (!nodeInfo)
-        return <Offline/>
-    if (error) {
-        //TODO adding an error view
-        show(true, `Error: ${error.toString()}`);
-        return <></>
-    }
-    if (!data)
-        return <Loading/>
-    let metricsOne: any = data.metrics["metric_one"]
-    console.debug(`Metric one with the follow payload ${metricsOne.toString()}`);
+export default function MetricsView({
+  nodeInfo,
+  show,
+}: MetricsViewProps): JSX.Element {
+  const { data, error } = useSWR("/api/metrics", fetcher);
+  if (!nodeInfo) return <Offline />;
+  if (error) {
+    //TODO adding an error view
+    show(true, `Error: ${error.toString()}`);
+    return <></>;
+  }
+  if (!data) return <Loading />;
+  let metricsOne: any = data.metrics["metric_one"];
+  console.debug(`Metric one with the follow payload ${metricsOne.toString()}`);
 
-    return <Box mt={theme.spacing(1)} mb={theme.spacing(2)}><Grid
+  return (
+    <Box mt={theme.spacing(1)} mb={theme.spacing(2)}>
+      <Grid
         container
-        style={{marginTop: "5em"}}
+        style={{ marginTop: "5em" }}
         direction="row"
         justifyContent="center"
         alignItems="center"
-    >
+      >
         <Grid item xs={12} xl={12} sm={12}>
-            <SummaryChannels nodeInfo={nodeInfo} metricsOne={metricsOne} show={show}/>
+          <SummaryChannels
+            nodeInfo={nodeInfo}
+            metricsOne={metricsOne}
+            show={show}
+          />
         </Grid>
 
         <Grid item xs={12} xl={12} sm={12}>
-            <UpTimeNode nodeInfo={nodeInfo} metricsOne={metricsOne} show={show}/>
+          <UpTimeNode nodeInfo={nodeInfo} metricsOne={metricsOne} show={show} />
         </Grid>
 
         <Grid item xs={12} xl={12} sm={12}>
-            <ChannelsPayments nodeInfo={nodeInfo} metricsOne={metricsOne} show={show}/>
+          <ChannelsPayments
+            nodeInfo={nodeInfo}
+            metricsOne={metricsOne}
+            show={show}
+          />
         </Grid>
-    </Grid>
+      </Grid>
     </Box>
+  );
 }
--- components/metricsView/cards/ChannelsPayments.component.tsx
+++ components/metricsView/cards/ChannelsPayments.component.tsx
@@ -1,16 +1,16 @@
-import React, { ReactElement } from 'react';
-import { GetInfoNode } from '../../../model/GetInfoNode';
-import { MetricsOne } from '../../../model/Metrics';
-import { Card, Grid } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
-import TodayRounded from '@mui/icons-material/TodayRounded';
-import CardHeader from '@mui/material/CardHeader';
-import { RadarProps, ResponsiveRadar as Radar } from '@nivo/radar';
-import CardContent from '@mui/material/CardContent';
-import makeTheme from '../../../theme/ChartTheme';
-import theme from '../../../theme/DarkTheme';
-import styles from '../../../styles/SummaryChannels.module.css';
-import { metricsOneToPaymentsContributionByChannels } from '../../../utils/AppUtils';
+import React, { ReactElement } from "react";
+import { GetInfoNode } from "../../../model/GetInfoNode";
+import { MetricsOne } from "../../../model/Metrics";
+import { Card, Grid } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import TodayRounded from "@mui/icons-material/TodayRounded";
+import CardHeader from "@mui/material/CardHeader";
+import { RadarProps, ResponsiveRadar as Radar } from "@nivo/radar";
+import CardContent from "@mui/material/CardContent";
+import makeTheme from "../../../theme/ChartTheme";
+import theme from "../../../theme/DarkTheme";
+import styles from "../../../styles/SummaryChannels.module.css";
+import { metricsOneToPaymentsContributionByChannels } from "../../../utils/AppUtils";
 
 interface ResponsiveRadarProps extends RadarProps {
   motionConfig?: string;
@@ -25,7 +25,11 @@ type ChannelsPaymentsProps = {
   show: (show: boolean, message: string) => void;
 };
 
-export default function ChannelsPayments({ nodeInfo, metricsOne, show }: ChannelsPaymentsProps): ReactElement {
+export default function ChannelsPayments({
+  nodeInfo,
+  metricsOne,
+  show,
+}: ChannelsPaymentsProps): ReactElement {
   let { color } = metricsOne;
   color = `#${color}`;
   let { data, labels } = metricsOneToPaymentsContributionByChannels(metricsOne);
@@ -37,56 +41,61 @@ export default function ChannelsPayments({ nodeInfo, metricsOne, show }: Channel
     <Card>
       <CardHeader
         avatar={
-          <Avatar aria-label='recipe'>
+          <Avatar aria-label="recipe">
             <TodayRounded />
           </Avatar>
         }
         title={`Forwards Payments by each channels`.toUpperCase()}
       />
       <CardContent>
-        <Grid container direction='row' justifyContent='center' alignItems='center'>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
           <Grid item xs={12} xl={12} sm={12}>
             <div className={styles.container}>
               <ResponsiveRadar
                 data={data}
                 keys={labels}
-                indexBy='node'
-                maxValue='auto'
-                curve='linearClosed'
+                indexBy="node"
+                maxValue="auto"
+                curve="linearClosed"
                 borderWidth={2}
                 margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
                 gridLevels={6}
-                gridShape='circular'
+                gridShape="circular"
                 gridLabelOffset={36}
                 enableDots={true}
                 dotSize={10}
-                colors={{ scheme: 'red_yellow_green' }}
-                dotColor={{ from: 'color', modifiers: [] }}
+                colors={{ scheme: "red_yellow_green" }}
+                dotColor={{ from: "color", modifiers: [] }}
                 dotBorderWidth={2}
-                dotBorderColor={{ from: 'color' }}
+                dotBorderColor={{ from: "color" }}
                 enableDotLabel={true}
-                dotLabel='value'
+                dotLabel="value"
                 dotLabelYOffset={-12}
                 fillOpacity={0.25}
-                blendMode='multiply'
+                blendMode="multiply"
                 animate={true}
-                motionConfig='wobbly'
+                motionConfig="wobbly"
                 isInteractive={true}
                 theme={makeTheme(color, theme.palette.text.primary)}
                 legends={[
                   {
-                    anchor: 'top-left',
-                    direction: 'column',
+                    anchor: "top-left",
+                    direction: "column",
                     translateX: -50,
                     translateY: -40,
                     itemWidth: 80,
                     itemHeight: 20,
                     itemTextColor: color,
                     symbolSize: 12,
-                    symbolShape: 'circle',
+                    symbolShape: "circle",
                     effects: [
                       {
-                        on: 'hover',
+                        on: "hover",
                         style: {
                           itemTextColor: color,
                         },
