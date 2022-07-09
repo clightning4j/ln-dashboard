52c346685e83c958bc225cd1f565c9512f42a802
gautamjajoo <gautamjajoo1729@gmail.com>
2022-07-09 19:09:30 +0530

    create one client

--- pages/api/offer.ts
+++ pages/api/offer.ts
@@ -2,16 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 import AppAPI from "../../api/AppAPI";
 import MockAPI from "../../api/MockAPI";
 
+let api: AppAPI = new MockAPI()
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    let api: AppAPI = new MockAPI();
-
     let resp = api.getOfferInfo();
-
-    console.debug(api.getOfferInfo());
+    console.debug(resp);
     res.status(200).json({ offer: resp });
   } catch (error) {
     console.error(error);
