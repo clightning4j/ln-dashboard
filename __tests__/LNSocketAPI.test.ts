bc9e72adbb62f3e5ba43877504ac3dab0686fae9
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-07-15 10:44:27 +0000

    ci: fixed ci conf
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- __tests__/LNSocketAPI.test.ts
+++ __tests__/LNSocketAPI.test.ts
@@ -5,11 +5,14 @@ test("Configure lnsocket", async () => {
   let rpcClient = new SocketApiClient(process.env.CLN_UNIX!);
   let getInfoRpc = await rpcClient.call<Map<string, unknown>>("getinfo", {});
   let nodeID = getInfoRpc.get("id");
-  let runeRPC: any = await rpcClient.call<Map<string, unknown>>("commando-rune", {"restrictions": "readonly"});
+  let runeRPC: any = await rpcClient.call<Map<string, unknown>>(
+    "commando-rune",
+    { restrictions: "readonly" }
+  );
   let client = new LNSocketAPI(
     /*node id*/ nodeID as string,
     /*address*/ "127.0.0.1:19735",
-    /*rune*/ runeRPC.get("rune") as string,
+    /*rune*/ runeRPC.get("rune") as string
   );
   await client.connect();
   let getInfo = await client.getInfo();
