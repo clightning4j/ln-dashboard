3fcac2f58df21997f4247d59cd4264f7b1826256
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-08-06 01:31:38 +0000

    reproduce problem with styled component and SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,43 +4,62 @@ import "reflect-metadata";
 import { StyledEngineProvider, ThemeProvider } from "@mui/system";
 import theme from "../theme/DarkTheme";
 import { CssBaseline } from "@mui/material";
+import { CacheProvider, EmotionCache } from "@emotion/react";
 import { Helmet } from "react-helmet";
 import BasicAppBar from "../components/appbar/BasicAppBar.component";
+import createEmotionCache from "../theme/createEmotionCache";
 
-function MyApp({ Component, pageProps }: AppProps) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Helmet>
-          <meta charSet="utf-8" />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="theme.palette.background.default" />
-          <title>Home</title>
-          <link rel="canonical" href="https://bruce.bublina.eu.org/" />
-        </Helmet>
-        <BasicAppBar ready={true} child={<Component {...pageProps} />} />
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <CacheProvider value={emotionCache}>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Helmet>
+            <meta charSet="utf-8" />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link
+              rel="mask-icon"
+              href="/safari-pinned-tab.svg"
+              color="#5bbad5"
+            />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta
+              name="theme-color"
+              content="theme.palette.background.default"
+            />
+            <title>Home</title>
+            <link rel="canonical" href="https://bruce.bublina.eu.org/" />
+          </Helmet>
+          <BasicAppBar ready={true} child={<Component {...pageProps} />} />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </CacheProvider>
   );
 }
 export default MyApp;
